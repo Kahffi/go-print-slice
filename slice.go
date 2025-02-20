92109379f0cde33241241f2deed6dc1b5f845ea2
Kahffi <daffaalkahffi24@gmail.com>
2025-02-21 00:08:11 +0700

    reducing tab

--- slice.go
+++ slice.go
@@ -5,7 +5,7 @@ import "fmt"
 func PrintSlice(slice []int, label string) {
 	fmt.Println(label)
 	for _, val := range slice {
-		fmt.Printf("\t%d\t", val)
+		fmt.Printf("%d\t", val)
 	}
 	fmt.Println()
 
@@ -15,7 +15,7 @@ func PrintSlice2D(slice [][]int, label string) {
 	fmt.Println(label)
 	for _, rows := range slice {
 		for _, val := range rows {
-			fmt.Printf("\t%d\t", val)
+			fmt.Printf("%d\t", val)
 		}
 		fmt.Println()
 	}
