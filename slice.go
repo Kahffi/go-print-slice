b5983247b70347dff8e0d48f53f7dd12e6249f08
Kahffi <daffaalkahffi24@gmail.com>
2025-02-20 11:17:40 +0700

    adding label and formatting

--- slice.go
+++ slice.go
@@ -2,19 +2,23 @@ package slice
 
 import "fmt"
 
-func PrintSlice(slice []int) {
+func PrintSlice(slice []int, label string) {
+	fmt.Println(label)
 	for _, val := range slice {
 		fmt.Printf("%d ", val)
 	}
+	fmt.Println()
 
 }
 
-func PrintSlice2D(slice [][]int) {
+func PrintSlice2D(slice [][]int, label string) {
+	fmt.Println(label)
 	for _, rows := range slice {
 		for _, val := range rows {
 			fmt.Printf("%d ", val)
 		}
 		fmt.Println()
 	}
+	fmt.Println()
 
 }
