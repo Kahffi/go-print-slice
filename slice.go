de0a51438b3f80ee18f6bb001f75858be75009e4
Kahffi <daffaalkahffi24@gmail.com>
2025-02-25 15:16:18 +0700

    formatting

--- slice.go
+++ slice.go
@@ -15,7 +15,11 @@ func PrintSlice2D(slice [][]int, label string) {
 	fmt.Println(label)
 	for _, rows := range slice {
 		for _, val := range rows {
-			fmt.Printf("%d  ", val)
+			if val > 0 {
+				fmt.Printf("%d\t", val)
+			} else {
+				fmt.Print(".\t")
+			}
 		}
 		fmt.Println()
 	}
